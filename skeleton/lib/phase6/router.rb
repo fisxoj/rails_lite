5165973cd7166bb629381a99cd3dbb3cfe5a64de
= <=>
2014-10-07 16:04:59 -0400

    Working router! Come one, come all!

--- skeleton/lib/phase6/router.rb
+++ skeleton/lib/phase6/router.rb
@@ -3,15 +3,25 @@ module Phase6
     attr_reader :pattern, :http_method, :controller_class, :action_name
 
     def initialize(pattern, http_method, controller_class, action_name)
+      @pattern, @http_method = pattern, http_method
+      @controller_class, @action_name = controller_class, action_name
     end
 
     # checks if pattern matches path and method matches request method
     def matches?(req)
+      !!pattern.match(req.path)
     end
 
     # use pattern to pull out route params (save for later?)
     # instantiate controller and call controller action
     def run(req, res)
+      if matches?(req)
+        puts "MATCHED"
+        params = Phase5::Params.new(req)
+        controller = controller_class.new(req, res, {})
+
+        controller.invoke_action(req.request_method)
+      end
     end
   end
 
@@ -19,28 +29,42 @@ module Phase6
     attr_reader :routes
 
     def initialize
+      @routes = []
     end
 
     # simply adds a new route to the list of routes
     def add_route(pattern, method, controller_class, action_name)
+      @routes << Route.new(pattern, method, controller_class, action_name)
     end
 
     # evaluate the proc in the context of the instance
     # for syntactic sugar :)
     def draw(&proc)
+      proc.call
     end
 
     # make each of these methods that
     # when called add route
     [:get, :post, :put, :delete].each do |http_method|
+      define_method(http_method) do |pattern, controller_class, action_name|
+        add_route(pattern, http_method, controller_class, action_name)
+      end
     end
 
     # should return the route that matches this request
     def match(req)
+      routes.find { |route| route.matches?(req) }
     end
 
     # either throw 404 or call run on a matched route
     def run(req, res)
+      route = match(req)
+
+      if route
+        route.run(req, res)
+      else
+        res.status = 404
+      end
     end
   end
 end
